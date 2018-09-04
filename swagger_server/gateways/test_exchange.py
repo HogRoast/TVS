d1ac9b9cc8dd6d064b47022d85c1806f14e747dc
Stuart <monkeeferret@gmail.com>
2018-09-04 16:39:25 +0100

    Further fleshed out the Test Exchange gwy and tsts

--- swagger_server/gateways/test_exchange.py
+++ swagger_server/gateways/test_exchange.py
@@ -4,6 +4,18 @@ class InvalidSessionError(Exception):
     def __init__(self, msg):
         self.msg = msg
 
+class InvalidAccountError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+class AccountPermissionError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+class OrderValidationError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
 # decorator to ensure applicable methods have a valid session when called
 def connected(fn):
     def wrapper(*args, **kwargs):
@@ -13,6 +25,11 @@ def connected(fn):
     return wrapper
 
 class TestGwy:
+    terminalOrderStates = ('rejected', 'filled', 'cancelled')
+    marketSides = ('buy', 'sell')
+    orderTypes = ('limit', 'market', 'stop', 'stoplimit')
+    durationTypes = ('gtt', 'gtd', 'gtc')
+
     def __init__(self):
         self.config = configparser.RawConfigParser()
         # overriding optionxform to ensure case sensitivity in config keys
@@ -23,6 +40,13 @@ class TestGwy:
         for account in self.config['account.details']:
             self.accounts[account] = eval(self.config['account.details'][account])
         self.session = None
+        self.numberFountains = {    'order'      : 126,
+                                    'execution'  : 1226 }
+
+    def getNextId(self, fountain):
+        id_ = self.numberFountains[fountain]
+        self.numberFountains[fountain] = id_ + 1
+        return id_
 
     def connect(self):
         if self.session:
@@ -42,22 +66,162 @@ class TestGwy:
 
     @connected
     def getPermissions(self, accountId):
-        return self.session['permissions'][accountId]
+        try:
+            return self.session['permissions'][accountId]
+        except KeyError:
+            raise InvalidAccountError('Account - ' + accountId + ' - does not exist')
 
     @connected
     def getInstruments(self, accountId):
         allInsts = self.getAllInstruments()
         permInsts = self.getPermissions(accountId)['instruments']
         insts = []
-        for inst in allInsts:
-            if inst['name'] in permInsts:
-                insts.append(inst)
-                # break out early if we have all the permed insts already
-                if len(insts) == len(permInsts):
-                    return insts
+        for inst in permInsts:
+            if inst in allInsts:
+                insts.append(allInsts[inst])
         return insts
 
     @connected
     def getExchangeInfo(self):
         return self.session['exchange_info']
+
+    @connected
+    def getExecutions(self, accountId, instrument, maxCount=None):
+        exes = []
+        cnt = 0
+        try:
+            userExes = self.session['executions'][accountId]
+            for exe in userExes:
+                if exe['instrument'] == instrument:
+                    exes.append(exe)
+                    cnt += 1
+                    if maxCount == cnt:
+                        break
+        except KeyError:
+            pass
+        return exes
+
+    @connected
+    def getOrders(self, accountId):
+        ords = []
+        try:
+            userOrds = self.session['orders'][accountId]
+            for order in userOrds:
+                # only collect those order that are not terminal
+                if order['status'] not in TestGwy.terminalOrderStates:
+                    ords.append(order)
+        except KeyError:
+            pass
+        return ords
+
+    @connected
+    def getOrder(self, accountId, orderId):
+        try:
+            userOrds = self.session['orders'][accountId]
+            for order in userOrds:
+                if order['id'] == orderId:
+                    return order
+        except KeyError:
+            pass
+        return None
+
+    @connected
+    def getHistoricalOrders(self, accountId, maxCount=None):
+        ords = []
+        cnt = 0
+        try:
+            userOrds = self.session['orders'][accountId]
+            for order in userOrds:
+                print(order['status'])
+                if order['status'] in TestGwy.terminalOrderStates:
+                    ords.append(order)
+                    cnt += 1
+                    if maxCount == cnt:
+                        break
+        except KeyError:
+            pass
+        return ords
+
+    @connected
+    def cancelOrder(self, accountId, orderId):
+        ords = self.getOrders(accountId)
+        for order in ords:
+            if order['id'] == orderId:
+                order['status'] = 'cancelled'
+                return True
+        return False
+
+    @connected
+    def modifyOrder(self, accountId, orderId, qty, limitPrice=None, stopPrice=None, stopLoss=None, takeProfit=None, digitalSignature=None):
+        # NOTE: digitialSignature, stopLoss and takeProfit are odd, as they
+        # are not supported by the swagger Order class, leaving them here as 
+        # optional params but will ignore for now.
+        # Also, stopLoss and takeProfit are not obvious parameters for a 
+        # single order in any case, so not quite sure what the aim was.
+        ords = self.getOrders(accountId)
+        for order in ords:
+            if order['id'] == orderId:
+                order['qty'] = qty;
+                if limitPrice is not None: 
+                    order['limit_price'] = limitPrice
+                if stopPrice is not None: 
+                    order['stop_price'] = stopPrice
+                return True
+        return False
+
+    @connected
+    def createOrder(self, accountId, instrument, qty, side, type_, limitPrice=None, stopPrice=None, durationType=None, durationDateTime=None, stopLoss=None, takeProfit=None, digitalSignature=None, requestId=None):
+        # See comment above regarding digitialSignature, stopLoss and takeProfit
+        # Also, ignoring requestId for now.
+        perms = self.getPermissions(accountId)
+        if instrument not in perms['instruments']:
+            raise AccountPermissionError('Account - ' + accountId + ' - is not permissioned for instrument: ' + instrument)
+
+        order = {}
+        order['id'] = str(self.getNextId('order'))
+        order['instrument'] = instrument
+        order['qty'] = qty
+        order['side'] = side
+        order['type'] = type_
+        order['filled_qty'] = 0.0
+        order['avg_price'] = None
+        order['limit_price'] = limitPrice
+        order['stop_price'] = stopPrice
+        order['parent_id'] = None
+        order['parent_type'] = None
+        order['duration_type'] = durationType
+        order['duration_datetime'] = durationDateTime
+        order['status'] = 'placing'
+        self.validateOrder(order)
+
+        orders = []
+        try:
+            orders = self.session['orders'][accountId]
+        except KeyError:
+            self.session['orders'][accountId] = orders
+        orders.append(order)
+        return order['id']
+
+    def validateOrder(self, order):
+        if 'id' not in order:
+            raise OrderValidationError('Order missing "id" field')
+        if 'instrument' not in order:
+            raise OrderValidationError('Order - ' + order['id'] + ' - missing "instrument" field')
+        if 'qty' not in order:
+            raise OrderValidationError('Order - ' + order['id'] + ' - missing "qty" field')
+        if 'side' not in order:
+            raise OrderValidationError('Order - ' + order['id'] + ' - missing "side" field')
+        if 'type' not in order:
+            raise OrderValidationError('Order - ' + order['id'] + ' - missing "type" field')
+        if 'limit_price' not in order:
+            raise OrderValidationError('Order - ' + order['id'] + ' - missing "limit_price" field')
+
+        if order['instrument'] not in self.getAllInstruments():
+            raise OrderValidationError('Order - ' + order['id'] + ' - has invalid "instrument": ' + order['instrument'])
+        if order['qty'] <= 0.0:
+            raise OrderValidationError('Order - ' + order['id'] + ' - has invalid "qty": ' + str(order['qty']))
+        if order['side'] not in TestGwy.marketSides:
+            raise OrderValidationError('Order - ' + order['id'] + ' - has invalid "side": ' + order['side'])
+        if order['type'] not in TestGwy.orderTypes:
+            raise OrderValidationError('Order - ' + order['id'] + ' - has invalid "type": ' + order['type'])
 
--- swagger_server/test/test_test_exchange_gateway.py
+++ swagger_server/test/test_test_exchange_gateway.py
@@ -1,4 +1,4 @@
-from swagger_server.gateways.test_exchange import TestGwy, InvalidSessionError
+from swagger_server.gateways.test_exchange import TestGwy, InvalidSessionError, InvalidAccountError, AccountPermissionError, OrderValidationError
 from swagger_server.test import BaseTestCase
 
 class TestTestExchangeGateway(BaseTestCase):
@@ -33,6 +33,13 @@ class TestTestExchangeGateway(BaseTestCase):
         self.assertEqual(len(perms), 1)
         self.assertEqual(perms['instruments'][0], 'TestInst1')
 
+    def test_getPermissions_NoAccount(self):
+        gwy = TestGwy()
+        gwy.connect()
+        with self.assertRaises(InvalidAccountError) as cm:
+            perms = gwy.getPermissions('NoSuchAccount')
+        self.assertEqual(cm.exception.msg, 'Account - NoSuchAccount - does not exist')
+
     def test_getPermissions_NotConnected(self):
         gwy = TestGwy()
         with self.assertRaises(InvalidSessionError) as cm:
@@ -43,9 +50,11 @@ class TestTestExchangeGateway(BaseTestCase):
         gwy = TestGwy()
         gwy.connect()
         insts = gwy.getAllInstruments()
-        self.assertEqual(len(insts), 2)
-        self.assertEqual(insts[0]['name'], 'TestInst1')
-        self.assertEqual(insts[1]['name'], 'TestInst2')
+        self.assertEqual(len(insts.keys()), 2)
+        self.assertTrue('TestInst1' in insts)
+        self.assertTrue('TestInst2' in insts)
+        self.assertEqual(insts['TestInst1']['name'], 'TestInst1')
+        self.assertEqual(insts['TestInst2']['name'], 'TestInst2')
 
     def test_getAllInstruments_NotConnected(self):
         gwy = TestGwy()
@@ -59,6 +68,13 @@ class TestTestExchangeGateway(BaseTestCase):
         insts = gwy.getInstruments('accountId_example')
         self.assertEqual(len(insts), 1)
         self.assertEqual(insts[0]['name'], 'TestInst1')
+
+    def test_getInstruments_NoAccount(self):
+        gwy = TestGwy()
+        gwy.connect()
+        with self.assertRaises(InvalidAccountError) as cm:
+            insts = gwy.getInstruments('NoSuchAccount')
+        self.assertEqual(cm.exception.msg, 'Account - NoSuchAccount - does not exist')
 
     def test_getInstruments_NotConnected(self):
         gwy = TestGwy()
@@ -77,6 +93,344 @@ class TestTestExchangeGateway(BaseTestCase):
         gwy = TestGwy()
         with self.assertRaises(InvalidSessionError) as cm:
             exInfo = gwy.getExchangeInfo()
+        self.assertEqual(cm.exception.msg, 'Not connected')
+
+    def test_getExecutions(self):
+        gwy = TestGwy()
+        gwy.connect()
+        exes = gwy.getExecutions('accountId_example', 'TestInst2')
+        self.assertEqual(len(exes), 1)
+        self.assertEqual(exes[0]['id'], '1225') 
+
+    def test_getExecutions_Multiple(self):
+        gwy = TestGwy()
+        gwy.connect()
+        exes = gwy.getExecutions('accountId_example', 'TestInst1')
+        self.assertEqual(len(exes), 2)
+        self.assertEqual(exes[0]['id'], '1223') 
+        self.assertEqual(exes[1]['id'], '1224') 
+
+    def test_getExecutions_MaxCount(self):
+        gwy = TestGwy()
+        gwy.connect()
+        exes = gwy.getExecutions('accountId_example', 'TestInst1', 1)
+        self.assertEqual(len(exes), 1)
+        self.assertEqual(exes[0]['id'], '1223') 
+
+    def test_getExecutions_EmptyList(self):
+        gwy = TestGwy()
+        gwy.connect()
+        exes = gwy.getExecutions('NoSuchAccount', 'TestInst2')
+        self.assertEqual(len(exes), 0)
+
+    def test_getExecutions_NotConnected(self):
+        gwy = TestGwy()
+        with self.assertRaises(InvalidSessionError) as cm:
+            exes = gwy.getExecutions('accountId_example', 'TestInst1')
+        self.assertEqual(cm.exception.msg, 'Not connected')
+
+    def test_getOrders(self):
+        gwy = TestGwy()
+        gwy.connect()
+        ords = gwy.getOrders('accountId_example')
+        self.assertEqual(len(ords), 1)
+        self.assertEqual(ords[0]['id'], '123')
+
+    def test_getOrders_EmptyList(self):
+        gwy = TestGwy()
+        gwy.connect()
+        ords = gwy.getOrders('NoSuchAccount')
+        self.assertEqual(len(ords), 0)
+
+    def test_getOrders_NotConnected(self):
+        gwy = TestGwy()
+        with self.assertRaises(InvalidSessionError) as cm:
+            ords = gwy.getOrders('accountId_example')
+        self.assertEqual(cm.exception.msg, 'Not connected')
+
+    def test_getHistoricalOrders(self):
+        gwy = TestGwy()
+        gwy.connect()
+        ords = gwy.getHistoricalOrders('accountId_example')
+        self.assertEqual(len(ords), 2)
+        self.assertEqual(ords[0]['id'], '124')
+        self.assertEqual(ords[1]['id'], '125')
+
+    def test_getHistoricalOrders_MaxCount(self):
+        gwy = TestGwy()
+        gwy.connect()
+        ords = gwy.getHistoricalOrders('accountId_example', 1)
+        self.assertEqual(len(ords), 1)
+        self.assertEqual(ords[0]['id'], '124')
+
+    def test_getHistoricalOrders_EmptyList(self):
+        gwy = TestGwy()
+        gwy.connect()
+        ords = gwy.getHistoricalOrders('NoSuchAccount')
+        self.assertEqual(len(ords), 0)
+
+    def test_getHistoricalOrders_NotConnected(self):
+        gwy = TestGwy()
+        with self.assertRaises(InvalidSessionError) as cm:
+            exes = gwy.getHistoricalOrders('accountId_example')
+        self.assertEqual(cm.exception.msg, 'Not connected')
+
+    def test_cancelOrder(self):
+        gwy = TestGwy()
+        gwy.connect()
+        self.assertTrue(gwy.cancelOrder('accountId_example', '123'))
+        ords = gwy.getHistoricalOrders('accountId_example')
+        cancelled = False
+        for order in ords:
+            if order['id'] == '123' and order['status'] == 'cancelled':
+                cancelled = True
+        self.assertTrue(cancelled)
+
+    def test_cancelOrder_NoOrder(self):
+        gwy = TestGwy()
+        gwy.connect()
+        self.assertFalse(gwy.cancelOrder('accountId_example', '789'))
+
+    def test_cancelOrder_NoAccount(self):
+        gwy = TestGwy()
+        gwy.connect()
+        self.assertFalse(gwy.cancelOrder('NoSuchAccount', '123'))
+
+    def test_cancelOrder_AlreadyTerminal(self):
+        gwy = TestGwy()
+        gwy.connect()
+        self.assertFalse(gwy.cancelOrder('accountId_example', '124'))
+
+    def test_cancelOrder_NotConnected(self):
+        gwy = TestGwy()
+        with self.assertRaises(InvalidSessionError) as cm:
+            gwy.cancelOrder('accountId_example', '123')
+        self.assertEqual(cm.exception.msg, 'Not connected')
+
+    def test_getOrder(self):
+        gwy = TestGwy()
+        gwy.connect()
+        order = gwy.getOrder('accountId_example', '123')
+        self.assertFalse(order is None)
+        self.assertEqual(order['id'], '123')
+
+    def test_getOrder_TermOrder(self):
+        gwy = TestGwy()
+        gwy.connect()
+        order = gwy.getOrder('accountId_example', '124')
+        self.assertFalse(order is None)
+        self.assertEqual(order['id'], '124')
+
+    def test_getOrder_NoOrder(self):
+        gwy = TestGwy()
+        gwy.connect()
+        order = gwy.getOrder('accountId_example', '343')
+        self.assertTrue(order is None)
+
+    def test_getOrder_NoAccount(self):
+        gwy = TestGwy()
+        gwy.connect()
+        order = gwy.getOrder('NoSuchAccount', '123')
+        self.assertTrue(order is None)
+
+    def test_getOrder_NotConnected(self):
+        gwy = TestGwy()
+        with self.assertRaises(InvalidSessionError) as cm:
+            order = gwy.getOrder('accountId_example', '123')
+        self.assertEqual(cm.exception.msg, 'Not connected')
+
+    def test_modifyOrder(self):
+        gwy = TestGwy()
+        gwy.connect()
+        self.assertTrue(gwy.modifyOrder('accountId_example', '123', 50.0, 100.05, 100.0))
+        order = gwy.getOrder('accountId_example', '123')
+        self.assertFalse(order is None)
+        self.assertEqual(order['qty'], 50.0)
+        self.assertEqual(order['limit_price'], 100.05)
+        self.assertEqual(order['stop_price'], 100.0)
+
+    def test_modifyOrder_NoOrder(self):
+        gwy = TestGwy()
+        gwy.connect()
+        self.assertFalse(gwy.modifyOrder('accountId_example', '343', 50.0, 100.05, 100.0))
+
+    def test_modifyOrder_NoAccount(self):
+        gwy = TestGwy()
+        gwy.connect()
+        self.assertFalse(gwy.modifyOrder('NoSuchAccount', '123', 50.0, 100.05, 100.0))
+
+    def test_modifyOrder_TermOrder(self):
+        gwy = TestGwy()
+        gwy.connect()
+        self.assertFalse(gwy.modifyOrder('accountId_example', '124', 50.0, 100.05, 100.0))
+
+    def test_modifyOrder_NotConnected(self):
+        gwy = TestGwy()
+        with self.assertRaises(InvalidSessionError) as cm:
+            gwy.modifyOrder('accountId_example', '123', 50.0)
+        self.assertEqual(cm.exception.msg, 'Not connected')
+
+    def test_getNextId(self):
+        gwy = TestGwy()
+        self.assertEqual(gwy.getNextId('order'), 126)
+        self.assertEqual(gwy.getNextId('order'), 127)
+        self.assertEqual(gwy.getNextId('execution'), 1226)
+        self.assertEqual(gwy.getNextId('execution'), 1227)
+
+    def test_getNextId_NoFountain(self):
+        gwy = TestGwy()
+        with self.assertRaises(KeyError) as cm:
+            gwy.getNextId('trevi')
+
+    def test_createOrder(self):
+        gwy = TestGwy()
+        gwy.connect()
+        id_ = gwy.createOrder('accountId_example', 'TestInst1', 12.0, 'buy', 'limit', 112.12, 112.0, 'gtt', 1002322.1)
+        self.assertEqual(id_, '126')
+        order = gwy.getOrder('accountId_example', id_)
+        self.assertFalse(order is None)
+        self.assertEqual(order['id'], '126')
+        self.assertEqual(order['instrument'], 'TestInst1')
+        self.assertEqual(order['qty'], 12.0)
+        self.assertEqual(order['side'], 'buy')
+        self.assertEqual(order['type'], 'limit')
+        self.assertEqual(order['filled_qty'], 0.0)
+        self.assertEqual(order['avg_price'], None)
+        self.assertEqual(order['limit_price'], 112.12)
+        self.assertEqual(order['stop_price'], 112.0)
+        self.assertEqual(order['parent_id'], None)
+        self.assertEqual(order['parent_type'], None)
+        self.assertEqual(order['duration_type'], 'gtt')
+        self.assertEqual(order['duration_datetime'], 1002322.1)
+        self.assertEqual(order['status'], 'placing')
+
+    def test_createOrder_NoAccount(self):
+        gwy = TestGwy()
+        gwy.connect()
+        with self.assertRaises(InvalidAccountError) as cm:
+            gwy.createOrder('NoSuchAccount', 'TestInst1', 12.0, 'buy', 'limit', 112.12, 112.0, 'gtt', 1002322.1)
+        self.assertEqual(cm.exception.msg, 'Account - NoSuchAccount - does not exist')
+
+    def test_createOrder_InstNotPermed(self):
+        gwy = TestGwy()
+        gwy.connect()
+        with self.assertRaises(AccountPermissionError) as cm:
+            gwy.createOrder('accountId_example', 'TestInst2', 12.0, 'buy', 'limit', 112.12, 112.0, 'gtt', 1002322.1)
+        self.assertEqual(cm.exception.msg, 'Account - accountId_example - is not permissioned for instrument: TestInst2')
+
+    def test_createOrder_NotConnected(self):
+        gwy = TestGwy()
+        with self.assertRaises(InvalidSessionError) as cm:
+            gwy.createOrder('accountId_example', 'TestInst1', 12.0, 'buy', 'limit', 112.12, 112.0, 'gtt', 1002322.1)
+        self.assertEqual(cm.exception.msg, 'Not connected')
+
+    def test_validateOrder(self):
+        gwy = TestGwy()
+        gwy.connect()
+        order = {}
+        order['id'] = '1'
+        order['instrument'] = 'TestInst1'
+        order['qty'] = 10.0
+        order['side'] = 'buy'
+        order['type'] = 'limit'
+        order['limit_price'] = 100.0
+        gwy.validateOrder(order)
+
+    def test_validateOrder_MissingId(self):
+        gwy = TestGwy()
+        gwy.connect()
+        order = {}
+        order['instrument'] = 'TestInst1'
+        order['qty'] = 10.0
+        order['side'] = 'buy'
+        order['type'] = 'limit'
+        order['limit_price'] = 100.0
+        with self.assertRaises(OrderValidationError) as cm:
+            gwy.validateOrder(order)
+        self.assertEqual(cm.exception.msg, 'Order missing "id" field')
+
+    def test_validateOrder_MissingOtherMandatory(self):
+        gwy = TestGwy()
+        gwy.connect()
+        order = {}
+        order['id'] = '1'
+        with self.assertRaises(OrderValidationError) as cm:
+            gwy.validateOrder(order)
+        self.assertEqual(cm.exception.msg, 'Order - 1 - missing "instrument" field')
+
+        order['instrument'] = 'TestInst1'
+        with self.assertRaises(OrderValidationError) as cm:
+            gwy.validateOrder(order)
+        self.assertEqual(cm.exception.msg, 'Order - 1 - missing "qty" field')
+
+        order['qty'] = 10.0
+        with self.assertRaises(OrderValidationError) as cm:
+            gwy.validateOrder(order)
+        self.assertEqual(cm.exception.msg, 'Order - 1 - missing "side" field')
+
+        order['side'] = 'buy'
+        with self.assertRaises(OrderValidationError) as cm:
+            gwy.validateOrder(order)
+        self.assertEqual(cm.exception.msg, 'Order - 1 - missing "type" field')
+
+        order['type'] = 'limit'
+        with self.assertRaises(OrderValidationError) as cm:
+            gwy.validateOrder(order)
+        self.assertEqual(cm.exception.msg, 'Order - 1 - missing "limit_price" field')
+
+    def test_validateOrder_IncorrectValues(self):
+        gwy = TestGwy()
+        gwy.connect()
+        order = {}
+        order['id'] = '1'
+        order['instrument'] = 'TestInst3'
+        order['qty'] = -0.1
+        order['side'] = 'mid'
+        order['type'] = 'banana'
+        order['limit_price'] = 100.0
+        with self.assertRaises(OrderValidationError) as cm:
+            gwy.validateOrder(order)
+        self.assertEqual(cm.exception.msg, 'Order - 1 - has invalid "instrument": TestInst3')
+
+        order['instrument'] = 'TestInst2'
+        with self.assertRaises(OrderValidationError) as cm:
+            gwy.validateOrder(order)
+        self.assertEqual(cm.exception.msg, 'Order - 1 - has invalid "qty": -0.1')
+
+        order['qty'] = 10.0
+        with self.assertRaises(OrderValidationError) as cm:
+            gwy.validateOrder(order)
+        self.assertEqual(cm.exception.msg, 'Order - 1 - has invalid "side": mid')
+
+        order['side'] = 'sell'
+        with self.assertRaises(OrderValidationError) as cm:
+            gwy.validateOrder(order)
+        self.assertEqual(cm.exception.msg, 'Order - 1 - has invalid "type": banana')
+
+    def test_validateOrder_MissingQty(self):
+        gwy = TestGwy()
+        gwy.connect()
+        order = {}
+        order['id'] = '1'
+        order['instrument'] = 'TestInst1'
+        order['side'] = 'buy'
+        order['type'] = 'limit'
+        order['limit_price'] = 100.0
+        with self.assertRaises(OrderValidationError) as cm:
+            gwy.validateOrder(order)
+        self.assertEqual(cm.exception.msg, 'Order - 1 - missing "qty" field')
+
+    def test_validateOrder_NotConnected(self):
+        gwy = TestGwy()
+        order = {}
+        order['id'] = '1'
+        order['instrument'] = 'TestInst1'
+        order['qty'] = 10.0
+        order['side'] = 'buy'
+        order['type'] = 'limit'
+        order['limit_price'] = 100.0
+        with self.assertRaises(InvalidSessionError) as cm:
+            gwy.validateOrder(order)
         self.assertEqual(cm.exception.msg, 'Not connected')
 
 if __name__ == '__main__':
